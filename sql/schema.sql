5a4c50b49b457e75e7255544395e4f3b12f3a040
Eddy27-hub <shanbowang886@gmail.com>
2025-11-25 16:46:58 +0800

    schema is finished
    Added Employee_Work_On and Temp_Employee_Work_On tables with constraints. Removed several unused tables and added Applied_To table with foreign key constraints.

--- sql/schema.sql
+++ sql/schema.sql
@@ -126,118 +126,97 @@ CREATE TABLE IF NOT EXISTS Mid_Level_Manage_Activity (
   PRIMARY KEY ( Activity_Building, Activity_Floor, Activity_RoomNum, Activity_Time)
 );
 
+CREATE TABLE IF NOT EXISTS Employee_Work_On (
+  
+  Working_Time DATE NOT NULL,
+  Working_Building CHAR(20) NOT NULL,
+  Working_Floor INT NOT NULL,
+  Working_Room_number INT NOT NULL,
 
+  
+  Working_Worker_Ssn CHAR(20) NOT NULL,
+  CONSTRAINT working_worker_level CHECK (
+    Working_Worker_Ssn IN (SELECT Ssn 
+                           FROM Employee 
+                           WHERE Emp_Level = 'base_level worker')
+  ),
 
+  
+  PRIMARY KEY (Working_Time, Working_Building, Working_Floor, Working_Room_number, Working_Worker_Ssn),
+  
+  CONSTRAINT fk_working_activity_location FOREIGN KEY ( Working_Building,  Working_Floor,  Working_Room_number)
+  REFERENCES Activity (Activity_Building, Activity_Floor, Activity_RoomNum)
+  ON UPDATE CASCADE,
 
+  CONSTRAINT fk_working_activity_time FOREIGN KEY ( Working_Time)
+  REFERENCES Activity (Activity_Time)
+  ON UPDATE CASCADE,
 
+  CONSTRAINT fk_working_employee_Ssn FOREIGN KEY ( Working_Worker_Ssn)
+  REFERENCES Employee (Ssn)
+  ON UPDATE CASCADE
 
+);
 
+CREATE TABLE IF NOT EXISTS Temp_Employee_Work_On (
+  
+  Temp_Working_Time DATE NOT NULL,
+  Temp_Working_Building CHAR(20) NOT NULL,
+  Temp_Working_Floor INT NOT NULL,
+  Temp_Working_Room_number INT NOT NULL,
 
+  
+  Temp_Working_Worker_Ssn CHAR(20) NOT NULL,
+    
+  PRIMARY KEY (Temp_Working_Time, Temp_Working_Building, Temp_Working_Floor, Temp_Working_Room_number, Temp_Working_Worker_Ssn),
+  
+  CONSTRAINT fk_temp_working_activity_location FOREIGN KEY ( Temp_Working_Building,  Temp_Working_Floor,  Temp_Working_Room_number)
+  REFERENCES Activity (Activity_Building, Activity_Floor, Activity_RoomNum)
+  ON UPDATE CASCADE,
 
+  CONSTRAINT fk_temp_working_activity_time FOREIGN KEY ( Temp_Working_Time)
+  REFERENCES Activity (Activity_Time)
+  ON UPDATE CASCADE,
 
+  CONSTRAINT fk_temp_working_employee_Ssn FOREIGN KEY ( Temp_Working_Worker_Ssn)
+  REFERENCES Temporary_Employee (TempSsn)
+  ON UPDATE CASCADE
 
-CREATE TABLE IF NOT EXISTS temp_employees (
-  TempSsn CHAR(9) PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  company VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Applied_To (
+
+  Applied_Time DATE NOT NULL,
+  Applied_Building CHAR(20) NOT NULL,
+  Applied_Floor INT NOT NULL,
+  Applied_Room_number INT NOT NULL,
+  Applied_Reason CHAR(100) NOT NULL,
+    
+  PRIMARY KEY (Applied_Time DATE, Applied_Building,  Applied_Floor,  Applied_Room_number),
+  
+  CONSTRAINT fk_applied_location FOREIGN KEY (Applied_Building,  Applied_Floor,  Applied_Room_number)
+  REFERENCES Location (Building, Floor, Room_number)
+  ON UPDATE CASCADE,
+
+  CONSTRAINT fk_applied_time FOREIGN KEY ( Applied_Time)
+  REFERENCES Activity (Activity_Time)
+  ON UPDATE CASCADE,
 );
 
 
 
-CREATE TABLE IF NOT EXISTS activities (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  manager_id INT,
-  activity_date DATE,
-  activity_type VARCHAR(50),
-  description VARCHAR(255),
-  requires_chemical TINYINT DEFAULT 0,
-  result VARCHAR(255),
-  finish_time DATETIME
-);
-
-CREATE TABLE IF NOT EXISTS activity_employees (
-  activity_id INT,
-  employee_ssn CHAR(9),
-  PRIMARY KEY (activity_id, employee_ssn)
-);
 
 
 
-CREATE TABLE IF NOT EXISTS activity_temp_employees (
-  activity_id INT,
-  temp_employee_ssn CHAR(9),
-  PRIMARY KEY (activity_id, temp_employee_ssn)
-);
 
 
 
-CREATE TABLE IF NOT EXISTS dependents (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  employee_id INT NOT NULL UNIQUE,
-  name VARCHAR(100) NOT NULL,
-  relationship VARCHAR(50)
-);
-
-CREATE TABLE IF NOT EXISTS activity_locations (
-  activity_id INT NOT NULL,
-  location_id INT NOT NULL,
-  reason VARCHAR(255) NOT NULL,
-  PRIMARY KEY (activity_id, location_id)
-);
-
-ALTER TABLE employees
-  ADD COLUMN office_location_id INT NULL,
-  ADD CONSTRAINT fk_employees_office_loc FOREIGN KEY (office_location_id) REFERENCES locations(id);
 
 
 
-ALTER TABLE temp_employees
-  ADD CONSTRAINT fk_temp_employees_company FOREIGN KEY (company) REFERENCES contractor_companies(name);
-
-ALTER TABLE activity_employees
-  ADD CONSTRAINT fk_act_emp_activity FOREIGN KEY (activity_id) REFERENCES activities(id),
-  ADD CONSTRAINT fk_act_emp_employee FOREIGN KEY (employee_ssn) REFERENCES employees(ssn);
 
 
 
-ALTER TABLE activity_temp_employees
-  ADD CONSTRAINT fk_act_temp_activity FOREIGN KEY (activity_id) REFERENCES activities(id),
-  ADD CONSTRAINT fk_act_temp_temp FOREIGN KEY (temp_employee_ssn) REFERENCES temp_employees(TempSsn);
 
-CREATE TABLE IF NOT EXISTS supervise_temp_employees (
-  temp_employee_ssn CHAR(9) NOT NULL,
-  supervisor_ssn CHAR(9) NOT NULL,
-  PRIMARY KEY (temp_employee_ssn, supervisor_ssn),
-  UNIQUE KEY uniq_temp (temp_employee_ssn)
-);
 
-CREATE TABLE IF NOT EXISTS supervise_contractor_companies (
-  company_name VARCHAR(100) NOT NULL,
-  supervisor_ssn CHAR(9) NOT NULL,
-  PRIMARY KEY (company_name, supervisor_ssn),
-  UNIQUE KEY uniq_company (company_name)
-);
 
-ALTER TABLE supervise_temp_employees
-  ADD CONSTRAINT fk_sup_temp_emp FOREIGN KEY (temp_employee_ssn) REFERENCES temp_employees(TempSsn),
-  ADD CONSTRAINT fk_sup_temp_sup FOREIGN KEY (supervisor_ssn) REFERENCES employees(ssn);
-
-ALTER TABLE supervise_contractor_companies
-  ADD CONSTRAINT fk_sup_cc_company FOREIGN KEY (company_name) REFERENCES contractor_companies(name),
-  ADD CONSTRAINT fk_sup_cc_sup FOREIGN KEY (supervisor_ssn) REFERENCES employees(ssn);
-
-ALTER TABLE supervise
-  ADD CONSTRAINT fk_supervise_emp FOREIGN KEY (employee_id) REFERENCES employees(ssn),
-  ADD CONSTRAINT fk_supervise_sup FOREIGN KEY (supervisor_id) REFERENCES employees(ssn);
-
-ALTER TABLE dependents
-  ADD CONSTRAINT fk_dependents_employee FOREIGN KEY (employee_id) REFERENCES employees(ssn);
-
-ALTER TABLE activity_locations
-  ADD CONSTRAINT fk_act_loc_activity FOREIGN KEY (activity_id) REFERENCES activities(id),
-  ADD CONSTRAINT fk_act_loc_location FOREIGN KEY (location_id) REFERENCES locations(id);
-
--- application-level enforcement for one activity per location/date is implemented in ActivityDAO.add
-
-ALTER TABLE employees ADD COLUMN supervisor_ssn CHAR(9) NULL;
-ALTER TABLE employees ADD COLUMN supervisee_ssn CHAR(9) NULL;
