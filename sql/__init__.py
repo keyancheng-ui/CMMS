f62695e99da66f490d30e5f0e4bfc9c69dfd57c6
kb24 <keyan_cheng@icloud.com>
2025-11-26 15:15:48 +0800

    update __init__.py in sql directory

---
+++ sql/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+# get sql file paths & define variables for these file paths
+SQL_DIR = os.path.dirname(__file__)
+
+SCHEMA_FILE = os.path.join(SQL_DIR, 'schema.sql')
+TEST_DATA_FILE = os.path.join(SQL_DIR, 'test_data.sql')
+SAMPLE_QUERIES_FILE = os.path.join(SQL_DIR, 'sample_queries.sql')
+
+def get_schema():
+    with open(SCHEMA_FILE, 'r', encoding='utf-8') as file1:
+        return file1.read()
+def get_test_data():
+    """读取 test_data.sql 文件内容"""
+    with open(TEST_DATA_FILE, 'r', encoding='utf-8') as file2:
+        return file2.read()
+def get_sample_queries():
+    """读取 sample_queries.sql 文件内容"""
+    with open(SAMPLE_QUERIES_FILE, 'r', encoding='utf-8') as file3:
+        return file3.read()
+
+__all__ = ['get_schema', 'get_test_data', 'get_sample_queries']
